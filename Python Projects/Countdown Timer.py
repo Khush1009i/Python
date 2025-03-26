bd135dba0d5c44671bea9eee9fb8fbfdf688f525
Khush soni <sonikhush004@gmail.com>
2025-03-26 21:41:28 +0530

    count down

---
+++ Python Projects/Countdown Timer.py
@@ -0,0 +1,13 @@
+import time
+
+
+alarm_time = int(input("Enter time in seconds:"))
+
+for x in range(alarm_time, 0, -1):
+    seconds = x %60
+    minutes = int(x /60)  %60
+    hours   = int(x/3600) %24
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("Time's up...!")
